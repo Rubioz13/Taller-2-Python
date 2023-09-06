2150a502c6da5f5a801f3faabcdd23b02c354faa
Rubioz13 <141454229+Rubioz13@users.noreply.github.com>
2023-09-05 20:22:04 -0500

    Delete Punto2.py

--- Punto2.py
+++
@@ -1,9 +0,0 @@
-tiempo_total_minutos = 0
-while True:
-    tiempo_tramo = int(input("Ingresa el tiempo de viaje del tramo (en minutos), o 0 para terminar: "))
-    if tiempo_tramo == 0:
-        break
-    tiempo_total_minutos += tiempo_tramo
-horas = tiempo_total_minutos // 60
-minutos = tiempo_total_minutos % 60
-print("El tiempo total de viaje es: " + str(horas) + " horas " + str(minutos) + " minutos")
